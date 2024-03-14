c742135a59b9cce190a41ccd48d232d22b32b7a9
franksharpe <145439826+franksharpe@users.noreply.github.com>
2024-03-14 09:56:20 +0000

    done

--- task5.py
+++ task5.py
@@ -1,6 +1,6 @@
 defective_prob_u = 0.20  # Probability of receiving a defective item from Uzumaki Safety Suppliers
 defective_prob_k = 0.10  # Probability of receiving a defective item from Khairan Safety Company
-defective_prob_l = 0.15  # Probability of receiving a defective item from Leaf First-Aid Kits Ltd
+defective_prob_l = 0  # Probability of receiving a defective item from Leaf First-Aid Kits Ltd
 
 # (a) Probability of receiving a defective item from each supplier
 prob_defective_u = 0.50 * defective_prob_u  # Probability of receiving a defective item from Uzumaki Safety Suppliers
@@ -8,12 +8,12 @@ prob_defective_k = 0.20 * defective_prob_k  # Probability of receiving a defecti
 prob_defective_l = 0.30 * defective_prob_l  # Probability of receiving a defective item from Leaf First-Aid Kits Ltd
 
 total_defective_prob = prob_defective_u + prob_defective_k + prob_defective_l  # Total probability of receiving a defective item
-print("Probability of receiving a defective item:", total_defective_prob)
+print("Probability of receiving a defective item:", total_defective_prob*100)
 
 # (b) Probability that a defective item came from Uzumaki Safety Suppliers
 # Applying Bayes' theorem: P(Uzumaki | Defective) = P(Defective | Uzumaki) * P(Uzumaki) / P(Defective)
 prob_defective_from_u = prob_defective_u / total_defective_prob
-print("Probability that a defective item came from Uzumaki Safety Suppliers:", prob_defective_from_u)
+print("Probability that a defective item came from Uzumaki Safety Suppliers:", prob_defective_from_u*100)
 
 # (c) Discussion
 # Compare the total probability of receiving a defective item before and after introducing Leaf First-Aid Kits Ltd
