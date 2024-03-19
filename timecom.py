63f34779af79a880ea265fe853d962adecee71e1
franksharpe <145439826+franksharpe@users.noreply.github.com>
2024-03-19 17:43:51 +0000

    g

--- timecom.py
+++ timecom.py
@@ -1,28 +1,54 @@
 import matplotlib.pyplot as plt
 
-# Function to plot hypothetical quadratic graphs
-def plot_quadratic_graphs():
+# Function to plot time complexities for Task 1 and Task 2 algorithms
+def plot_time_complexities(task1_complexities, task2_complexities):
     # Number of orders
     num_orders = list(range(0, 1000, 50))  # Hypothetical range of number of orders
-    # Time complexities for different operations
-    loading_time = [n for n in num_orders]  # Linear loading time
-    saving_time = [n**2 for n in num_orders]  # Quadratic saving time
-    deleting_time = [n**2 for n in num_orders]  # Quadratic deleting time
-    modifying_time = [n for n in num_orders]  # Linear modifying time
 
     # Plotting
-    plt.figure(figsize=(10, 6))
-    plt.plot(num_orders, loading_time, label='Loading Orders (O(n))')
-    plt.plot(num_orders, saving_time, label='Saving Orders (O(n^2))')
-    plt.plot(num_orders, deleting_time, label='Deleting Orders (O(n^2))')
-    plt.plot(num_orders, modifying_time, label='Modifying Orders (O(n))')
+    plt.figure(figsize=(14, 6))
 
-    plt.title('Hypothetical Time Complexities')
+    # Plot Task 1 complexities
+    plt.subplot(1, 2, 1)
+    plt.plot(num_orders, task1_complexities['loading'], label='Loading Orders (O(n))')
+    plt.plot(num_orders, task1_complexities['saving'], label='Saving Orders (O(n^2))')
+    plt.plot(num_orders, task1_complexities['deleting'], label='Deleting Orders (O(n^2))')
+    plt.plot(num_orders, task1_complexities['modifying'], label='Modifying Orders (O(n))')
+    plt.title('Task 1 Algorithm Time Complexities')
     plt.xlabel('Number of Orders')
     plt.ylabel('Time (arbitrary units)')
     plt.legend()
     plt.grid(True)
+
+    # Plot Task 2 complexities
+    plt.subplot(1, 2, 2)
+    plt.plot(num_orders, task2_complexities['loading'], label='Loading Orders (O(n))')
+    plt.plot(num_orders, task2_complexities['saving'], label='Saving Orders (O(n^2))')
+    plt.plot(num_orders, task2_complexities['deleting'], label='Deleting Orders (O(n^2))')
+    plt.plot(num_orders, task2_complexities['modifying'], label='Modifying Orders (O(n))')
+    plt.title('Task 2 Algorithm Time Complexities')
+    plt.xlabel('Number of Orders')
+    plt.ylabel('Time (arbitrary units)')
+    plt.legend()
+    plt.grid(True)
+
+    plt.tight_layout()
     plt.show()
 
-# Plot the graphs
-plot_quadratic_graphs()
+# Define hypothetical time complexities for Task 1 and Task 2
+task1_complexities = {
+    'loading': [n for n in range(0, 1000, 50)],  # Linear loading time
+    'saving': [n**2 for n in range(0, 1000, 50)],  # Quadratic saving time
+    'deleting': [n**2 for n in range(0, 1000, 50)],  # Quadratic deleting time
+    'modifying': [n for n in range(0, 1000, 50)]  # Linear modifying time
+}
+
+task2_complexities = {
+    'loading': [n for n in range(0, 1000, 50)],  # Linear loading time
+    'saving': [n**2 for n in range(0, 1000, 50)],  # Quadratic saving time
+    'deleting': [n**2 for n in range(0, 1000, 50)],  # Quadratic deleting time
+    'modifying': [n for n in range(0, 1000, 50)]  # Linear modifying time
+}
+
+# Plot the time complexities
+plot_time_complexities(task1_complexities, task2_complexities)
