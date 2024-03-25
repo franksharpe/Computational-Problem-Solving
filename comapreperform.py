03d6e9a3421bdef8eef332f80413fff733c95235
franksharpe <145439826+franksharpe@users.noreply.github.com>
2024-03-25 15:47:41 +0000

    done

--- comapreperform.py
+++ comapreperform.py
@@ -23,8 +23,8 @@ line_numbers = list(range(1, len(original_memory_usage) + 1))
 
 # Plotting
 plt.figure(figsize=(10, 6))
-plt.plot(line_numbers, original_memory_usage, label='Original Version')
-plt.plot(line_numbers, modified_memory_usage, label='Modified Version')
+plt.plot(line_numbers, original_memory_usage, label='Task 1')
+plt.plot(line_numbers, modified_memory_usage, label='Task 2')
 plt.xlabel('Line Number')
 plt.ylabel('Memory Usage (MiB)')
 plt.title('Memory Usage Comparison')
